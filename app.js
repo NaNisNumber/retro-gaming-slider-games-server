51dbff2caf33744f1e9a6636604fe4199266a04d
Tobirama <grigoroscuta.sergiu@yahoo.com>
2023-06-02 00:13:45 +0300

    adding server

---
+++ app.js
@@ -0,0 +1,52 @@
+const request = require("request");
+const http = require("http");
+let randomSliderGames;
+
+const gameOptionsReq = {
+  url: "https://api.igdb.com/v4/games",
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Client-ID": "vb838qqqsndwhxbu1gg98ljvtavacr",
+    Authorization: "Bearer zkomcikvy6y3dqgffmw7osa8to49fm",
+  },
+  body: "fields cover.*;where cover!=null & themes != (42) & first_release_date <= 631152000 & rating != null;limit 400; ",
+};
+
+function generateRandomGameIndexes() {
+  let randomGames = [];
+  for (let i = 0; i <= 4; i++) {
+    const getRandomGameIndex = function () {
+      const randomGameIndex = Math.floor(Math.random() * 249 + 1);
+      return randomGameIndex;
+    };
+    let randomGameIndex = getRandomGameIndex();
+    if (randomGames.includes(randomGameIndex)) {
+      randomGameIndex = getRandomGameIndex();
+    }
+    randomGames.push(randomGameIndex);
+  }
+
+  return randomGames;
+}
+
+const getSliderGameData = (gameData) => {
+  const sliderGamesData = randomGameIndexes.map((randomGameIndex) => {
+    const sliderGameImgUrl = gameData[randomGameIndex].cover.url.replace(
+      "thumb",
+      "cover_big"
+    );
+    const gameId = gameData[randomGameIndex].id;
+    return { id: gameId, imgUrl: sliderGameImgUrl };
+  });
+  return sliderGamesData;
+};
+const randomGameIndexes = generateRandomGameIndexes();
+
+request(gameOptionsReq, function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    const parsedApiData = JSON.parse(body);
+    const sliderGamesData = getSliderGameData(parsedApiData);
+    randomSliderGames = sliderGamesData;
+  }
+});
